ebe6de8f271a041471d6316af6647bdac828e17f
deliailu27 <54033364+deliailu27@users.noreply.github.com>
2022-08-31 21:24:59 +0100

    complete

---
+++ prisma/migrations/20220831195946_movie_screening/migration.sql
@@ -0,0 +1,29 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `contactId` on the `Customer` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Customer" DROP COLUMN "contactId";
+
+-- CreateTable
+CREATE TABLE "Movie" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "runtimeMins" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Movie_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Screening" (
+    "id" SERIAL NOT NULL,
+    "startsAt" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Screening_pkey" PRIMARY KEY ("id")
+);
--- prisma/seed.js
+++ prisma/seed.js
@@ -28,6 +28,33 @@ async function seed() {
 
   console.log("create customer and contact:", createdContactAndCustomer);
 
+  const createdMovie = await prisma.movie.create({
+    data: { title: "test", runtimeMins: 120 },
+  });
+
+  const createdScreen = await prisma.screen.create({
+    data: { number: 1 },
+  });
+
+  const date = new Date();
+  const Createdscreening = await prisma.screening.create({
+    data: {
+      startsAt: date,
+      movie: { connect: { id: createdMovie.id } },
+      screen: { connect: { id: createdScreen.id } },
+    },
+  });
+
+  console.log(Createdscreening);
+
+  const newTicket = await prisma.ticket.create({
+    data: {
+      customer: { connect: { id: createdCustomer.id } },
+      Screening: { connect: { id: Createdscreening.id } },
+    },
+  });
+  console.log("new ticket:", newTicket);
+
   // Don't edit any of the code below this line
   process.exit(0);
 }
